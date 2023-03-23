929bd96a43eb2462c57faed00c65b5a82e02962a
HenockAdane <adanehenock@gmail.com>
2023-03-23 16:05:33 +0000

    chaning path

--- api/routes/Auth.js
+++ api/routes/Auth.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const controller = require("../Controller/AuthController")
+const controller = require("api/controller/AuthController.js")
 const verifyJWT = require("../Controller/VerifyJWT")
 
 router.post("/signup", controller.signUp)
